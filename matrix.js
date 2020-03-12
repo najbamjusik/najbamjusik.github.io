d49556aee572f2e510deee27e92c9d8e3dbb4c11
kruligh <kruligh@DESKTOP-V3J9PPG.localdomain>
2020-03-12 23:21:52 +0100

    Make matrix playable and pausable

--- matrix.js
+++ matrix.js
@@ -5,9 +5,80 @@ const canvasHeight = canvas.height = document.body.offsetHeight;
 const columns = Math.floor(canvasWidth / 20) + 1;
 ctx.fillStyle = '#000';
 ctx.fillRect(0, 0, canvasWidth, canvasHeight);
-const audio = new Audio("resources/Krulig-Ballada_o_wszystkim.wav");
 
-async function drawText(dataToPrint, msDuration) {
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const hihatInterval = 1.5;
+
+const timeline = [
+    {text: "jo", timestamp: 2},
+    {text: "dominik krulig bugz", timestamp: 3},
+    {text: "najba mjusik", timestamp: 5},
+    {text: "??? ??? ???", timestamp: 8},
+    {text: "rzuciłem studia", timestamp: 12},
+    {text: "w chuj hajs", timestamp: 18},
+    {text: "do jutra", timestamp: 23},
+    {text: "do jutra", timestamp: 24},
+    {text: "z ziomami", timestamp: 26.2},
+    {text: "za rzadko spotykamy", timestamp: 28.6},
+    {text: "ziomy tęsknią", timestamp: 32.4},
+    {text: "swoją karierę", timestamp: 34.6},
+    {text: "w obce miasto", timestamp: 37.5},
+    {text: "nie widzimy się za czesto", timestamp: 40.4},
+    {text: "na pewno", timestamp: 47},
+    {text: "na pewno", timestamp: 47.8},
+    {text: "na ruchy przestzeń", timestamp: 53},
+    {text: "więcej", timestamp: 59},
+    {text: "więcej", timestamp: 60},
+    {text: "żałuję przed snem", timestamp: 64},
+    {text: "strach", timestamp: 72.8},
+    {text: "strachstrachstrachstrachstrachstrach", timestamp: 72.8 + 1 * hihatInterval},
+    {text: "     strach     ", timestamp: 72.8 + 2 * hihatInterval},
+    {text: "      strach          strach         strach     ", timestamp: 72.8 + 3 * hihatInterval},
+    {text: "overthinking", timestamp: 84.8},
+    {text: "over?overthinking?thinking", timestamp: 84.8 + 1 * hihatInterval},
+    {text: "~~~~~~overthinking~~~~~~", timestamp: 84.8 + 2 * hihatInterval},
+    {text: "~overthinking~~~~~~overthinking~~~~~~overthinking~", timestamp: 84.8 + 3 * hihatInterval},
+    // {text: "jebać depresje", timestamp: 22},
+    // {text: "strach", timestamp: 22},
+
+    {text: "", timestamp: 100}, // end item required
+];
+let currentDrawingTaskId = 0;
+
+async function run(drawingTaskId, startingTimestamp) {
+    const dataHeh = [
+        {text: " ", timestamp: startingTimestamp},
+        ...timeline.filter((entry) => entry.timestamp >= startingTimestamp)
+    ];
+    const audio = document.getElementById("audio-element");
+
+    while (1) {
+        audio.currentTime = startingTimestamp;
+        const startTime = Date.now();
+        for (const [idx, currentEntry] of dataHeh.entries()) {
+            const isLast = idx === dataHeh.length - 1;
+            if (isLast) {
+                continue;
+            }
+            const nextEntry = dataHeh[idx + 1];
+            const durationSec = nextEntry.timestamp - currentEntry.timestamp;
+            const duratonMs = Math.floor(durationSec * 1000);
+            console.log(`Time ${startingTimestamp + (Date.now() - startTime) / 1000} s, text:\n ${currentEntry.text}`);
+            await drawText(drawingTaskId, currentEntry.text, duratonMs);
+            if (currentDrawingTaskId > drawingTaskId) {
+                break;
+            }
+        }
+        if (currentDrawingTaskId > drawingTaskId) {
+            break;
+        }
+    }
+}
+
+async function drawText(drawingTaskId, dataToPrint, msDuration) {
     const startTime = Date.now();
     const ypositions = Array(columns).fill(0);
 
@@ -42,84 +113,28 @@ async function drawText(dataToPrint, msDuration) {
             } else {
                 ypositions[idx] = y + 20
             }
-            ;
         });
     }
 
     while (startTime + msDuration > Date.now()) {
         step();
+        if (currentDrawingTaskId > drawingTaskId) {
+            break;
+        }
         await sleep(50);
     }
 }
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+function musicPlaying() {
+    const startingTimestamp = 0;
+    const startTime = Date.now();
+    // setInterval(() => {
+    //     console.info(`Time ${startingTimestamp + (Date.now() - startTime) / 1000} s`);
+    // }, 1000);
+    currentDrawingTaskId++;
+    run(currentDrawingTaskId, startingTimestamp);
 }
 
-const startingTimestamp = 0;
-const hihatInterval = 1.5;
-const dataHeh = [
-    {text: " ", timestamp: startingTimestamp},
-    {text: "jo", timestamp: 2},
-    {text: "dominik krulig bugz", timestamp: 3},
-    {text: "najba mjusik", timestamp: 5},
-    {text: "??? ??? ???", timestamp: 8},
-    {text: "rzuciłem studia", timestamp: 12},
-    {text: "w chuj hajs", timestamp: 18},
-    {text: "do jutra", timestamp: 23},
-    {text: "do jutra", timestamp: 24},
-    {text: "z ziomami", timestamp: 26.2},
-    {text: "za rzadko spotykamy", timestamp: 28.6},
-    {text: "ziomy tęsknią", timestamp: 32.4},
-    {text: "swoją karierę", timestamp: 34.6},
-    {text: "w obce miasto", timestamp: 37.5},
-    {text: "nie widzimy się za czesto", timestamp: 40.4},
-    {text: "na pewno", timestamp: 47},
-    {text: "na pewno", timestamp: 47.8},
-    {text: "na ruchy przestzeń", timestamp: 53},
-    {text: "więcej", timestamp: 59},
-    {text: "więcej", timestamp: 60},
-    {text: "żałuję przed snem", timestamp: 64},
-    {text: "strach", timestamp: 72.8},
-    {text: "strachstrachstrachstrachstrachstrach", timestamp: 72.8 + 1 * hihatInterval},
-    {text: "     strach     ", timestamp: 72.8 + 2 * hihatInterval},
-    {text: "      strach          strach         strach     ", timestamp: 72.8 + 3 * hihatInterval},
-    {text: "overthinking", timestamp: 84.8},
-    {text: "over?overthinking?thinking", timestamp: 84.8 + 1 * hihatInterval},
-    {text: "~~~~~~overthinking~~~~~~", timestamp: 84.8 + 2 * hihatInterval},
-    {text: "~overthinking~~~~~~overthinking~~~~~~overthinking~", timestamp: 84.8 + 3 * hihatInterval},
-    // {text: "jebać depresje", timestamp: 22},
-    // {text: "strach", timestamp: 22},
-
-    {text: "", timestamp: 100}, // end item required
-].filter((entry) => entry.timestamp >= startingTimestamp);
-
-console.log(dataHeh);
-
-async function run() {
-    while (1) {
-        audio.currentTime = startingTimestamp;
-        await audio.play().catch((err) => {
-            console.error("Audio playing error");
-            console.error(err);
-        });
-        const startTime = Date.now();
-        setInterval(() => {
-            console.info(`Time ${startingTimestamp + (Date.now() - startTime) / 1000} s`);
-        }, 1000);
-        for (const [idx, currentEntry] of dataHeh.entries()) {
-            const isLast = idx === dataHeh.length - 1;
-            if (isLast) {
-                continue;
-            }
-            const nextEntry = dataHeh[idx + 1];
-            const durationSec = nextEntry.timestamp - currentEntry.timestamp;
-            const duratonMs = Math.floor(durationSec * 1000);
-            console.log(`Time ${startingTimestamp + (Date.now() - startTime) / 1000} s, text:\n ${currentEntry.text}`);
-            await drawText(currentEntry.text, duratonMs);
-        }
-        console.log('end');
-    }
+function musicStopped() {
+    currentDrawingTaskId++;
 }
-
-run();
